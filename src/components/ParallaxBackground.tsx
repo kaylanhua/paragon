a9ecf810f06a91b354c7be911bd93422764b9df3
George Parks <60108810+geoge23@users.noreply.github.com>
2023-12-27 19:50:51 -0500

    loop update

--- src/components/ParallaxBackground.tsx
+++ src/components/ParallaxBackground.tsx
@@ -6,26 +6,30 @@ import { animated, useScroll, useSpring } from "react-spring";
 const Y_OFFSET = -25;
 const X_OFFSET = -75;
 const DAMPER = 0.3;
+const FLOAT_TIME = 30000;
 
-export default function ParallaxBackground() {
-    const { scrollYProgress } = useScroll();
-    const floatDown = useSpring({
-        from: {
+// Was having problems with the parallax looping correctly, so a little reverse engineering
+// of the library led me to this hacky solution. Basically just a function that returns
+// an object with the correct properties for the loop, switching the from and to values
+// depending on the current state of the loop.
+function injectLoop(e: boolean) {
+    return {
+        loop: () => injectLoop(!e),
+        [e ? "from" : "to"]: {
             transform: "translateY(0%)"
         },
-        to: {
+        [e ? "to" : "from"]: {
             transform: "translateY(-100%)"
         },
         config: {
-            duration: 300000
+            duration: FLOAT_TIME
         },
-        loop: {
-            reverse: true,
-            config: {
-                duration: 1000
-            }
-        }
-    })
+    }
+}
+
+export default function ParallaxBackground() {
+    const { scrollYProgress } = useScroll();
+    const floatDown = useSpring(injectLoop(true));
 
     return <div className="h-screen w-screen fixed top-0 left-0 -z-50 overflow-hidden">
         <animated.div style={floatDown}>
