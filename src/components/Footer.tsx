60b1aea5c90d581a2c8c04becbffde0b55f94ad9
George Parks <60108810+geoge23@users.noreply.github.com>
2024-02-02 14:08:27 -0500

    improved footer placement

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,17 @@ import HeaderLogo from '@/assets/header-logo.svg';
 import Image from "next/image";
 
 export default function Footer({ children }: { children: React.ReactNode }) {
-    return <footer className='p-11 h-60 relative flex md:justify-start justify-center md:px-[25vw]' >
-        <div className='flex flex-col md:items-end'>
-            <Image src={HeaderLogo} alt="Paragon Logo" className='w-[250px]' />
-            <p className='text-gray-600 md:text-right'>Copyright © Paragon 2024</p>
-            <p className='text-gray-600 md:text-right'>Website by <a href="https://georgeparks.me">George Parks</a><br></br>and <a href="https://georgeparks.me">Will Varner</a></p>
-        </div>
-        <div className='md:flex hidden h-full'>
-            <div className='w-px h-full bg-gray-600 mx-14'></div>
-            {children}
+    return <footer className='p-11 h-60 relative flex justify-center' >
+        <div className='flex h-full md:w-[1150px]'>
+            <div className='flex flex-col md:items-end'>
+                <Image src={HeaderLogo} alt="Paragon Logo" className='w-[250px]' />
+                <p className='text-gray-600 md:text-right'>Copyright © Paragon 2024</p>
+                <p className='text-gray-600 md:text-right'>Website by <a href="https://georgeparks.me">George Parks</a><br></br>and <a href="https://georgeparks.me">Will Varner</a></p>
+            </div>
+            <div className='md:flex hidden h-full'>
+                <div className='w-px h-full bg-gray-600 mx-14'></div>
+                {children}
+            </div>
         </div>
         <div className='absolute -z-20 top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-darker' />
     </footer>
