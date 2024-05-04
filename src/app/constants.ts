d212798a41502681e18fcaafcaf97833b2dcb487
Will <willv678@gmail.com>
2024-05-04 13:31:45 -0400

    link change

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -76,7 +76,7 @@ export const colleges: {
 export const AIRTABLE_API_KEY = `patUaz6OGg90PIjOT.a3a57483b5bb0c36b546a85441b77f8737b4d61e679345b33804f29385d8ed16` // i realize this is bad practice but this key can only read from only the people table (which is public) so im not concerned
 export const AIRTABLE_BASE_ID = `appItWRxWaZWa1qhj`
 
-export const AIRTABLE_URL = "https://airtable.com/appRhVSa7OpK9YsDn/pagFfGYZK9UoorY0A/form"
+export const AIRTABLE_URL = "https://airtable.com/appItWRxWaZWa1qhj/shrfKG3W0XpO1L2tx"
 
 export type MenuOption = {
     name: string,
